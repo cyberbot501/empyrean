3e3bf866a9d7e9f9b1fc7bcc3bd7565a1a10b309
cyberbot501 <softwareengineerjoycy@gmail.com>
2023-11-06 17:20:48 +0100

    empyrean

---
+++ src/js/product.js
@@ -0,0 +1,18 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show-items");
+        }else{
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
