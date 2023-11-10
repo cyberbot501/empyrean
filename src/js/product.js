2463b55718420f5d4e3ba42789f6537058503e19
cyberbot501 <softwareengineerjoycy@gmail.com>
2023-11-10 23:01:34 +0100

    sizes

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,20 @@
-// let menuIcon = document.querySelector("#menu-icon");
-// let navlist = document.querySelector(".navlist");
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
 
-// menuIcon.onclick = ()=>{
-//     menuIcon.classList.toggle("bx-x");
-//     navlist.classList.toggle("open");
-// }
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
 
-// const observer = new IntersectionObserver((entries)=>{
-//     entries.forEach((entry)=>{
-//         if(entry.isIntersecting){
-//             entry.target.classList.add("show-items");
-//         }else{
-//             entry.target.classList.remove("show-items");
-//         }
-//     });
-// });
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show-items");
+        }else{
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
 
 
 
