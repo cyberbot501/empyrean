d0e9644186129eee8b913e183537b133a8c0b31a
cyberbot501 <softwareengineerjoycy@gmail.com>
2023-11-10 11:01:55 +0100

    yea

--- src/js/product.js
+++ src/js/product.js
@@ -16,128 +16,58 @@
 //     });
 // });
 
-// const productImageElement = document.getElementById("product-image");
-// const productNameElement = document.getElementById("product-name");
-// const productDescriptionElement = document.getElementById(
-//   "product-description"
-// );
-// const productPriceElement = document.getElementById("product-price");
 
-// Fetch product data from the API
-// fetch("https://empyrean.washrytelaundry.com.ng/api/v1/products", {
-//   method: "GET",
-// })
-//   .then((response) => {
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok");
-//     }
-//     return response.json();
-//   })
-//   .then((data) => {
-//     // Assuming the API response is an array of products
-//     if (data.length > 0) {
-//       // Display the first product in the array
-//       const firstProduct = data[0];
-//       productImageElement.src = firstProduct.image; // Update the product image source
-//       productNameElement.textContent = firstProduct.name;
-//       productDescriptionElement.textContent = firstProduct.description;
-//       productPriceElement.textContent = `Price: $${firstProduct.price}`;
-//     } else {
-//       productNameElement.textContent = "No products found";
-//       productDescriptionElement.textContent = "";
-//       productPriceElement.textContent = "";
-//     }
-//   })
-//   .catch((error) => {
-//     console.error("Error:", error);
-//   });
 
-// Display the product data on the page.
-document.getElementById('product-name').innerHTML = productData.name;
-document.getElementById('product-image').src = productData.attributes[0].variations[0].image; // Update to the correct image URL
-document.getElementById('product-description').textContent = productData.description;
 
-fetch('https://empyrean.washrytelaundry.com.ng/api/v1/products')
-.then((res) => {
-    if (res.ok) {
-        res
-        .json()
-        .then((res) => {
+const productcontainer= document.getElementById('product-containers');
+// const productNameElement = document.getElementById('product-name');
+// const productDescriptionElement = document.getElementById('product-description');
+// const productPriceElement = document.getElementById('product-price');
 
-            
-        
-
+fetch("http://empyrean.washrytelaundry.com.ng/api/v1/products",
+ {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+}).then((res) => {
+  if (res.ok) {
+    res
+      .json()
+      .then((res) => {
+        productcontainer
+        console.log(res)
+        res.data.map((item)=> {
+          let product = `<div class="products">
+          <div class="product-grid">
+              <img src="/public/asset/imges/Frame 21 (3).svg" alt="" id="product-image">
+              <h3 class="product-name" id="product-name">${item.name}</h3>
+              <p id="product-description">${item.description}</p>
+              <div class="star">
+                  <img src="/public/asset/imges/Star 1.svg" alt="">
+                  <img src="/public/asset/imges/Star 1.svg" alt="">
+                  <img src="/public/asset/imges/Star 1.svg" alt="">
+                  <img src="/public/asset/imges/Star 1.svg" alt="">
+                  <img src="/public/asset/imges/Star 5.svg" alt="">
+                  <img src="/public/asset/imges/Star 5.svg" alt="">
+              </div>
+              <h3 id="product-price">${item.price}</h3>
+              <button>ADD TO CART</button>
+          </div>
+      </div>`
+      // console.log(product)
+      productcontainer.innerHTML += product
         })
-    }
-
-    .catch((err) => {
-        // console.log(err);
-        let msg = "Something went wrong";
-        setErrorMessage(msg);
+        // localStorage.setItem("token", res.data.api_token);
+        // window.location.href = "/product_page";
+      })
+      .catch((err) => {
+        console.log(err);
+        // let msg = "Something went wrong";
+        // setErrorMessage(msg);
       });
-       
-})
-
-
-const form = document.querySelector("form");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const credentials = {
-    productName: e.target[0].value,
-    productDescription: e.target[1].value,
-    productPrice: e.target[2].value,
-  };
-
-  product(credentials);
+  }
 });
 
-function product(credentials) {
-  setErrorMessage("");
 
-  let errorMessage;
-  fetch("http://empyrean.washrytelaundry.com.ng/api/v1/auth/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(credentials),
-  })
-    .then((res) => {
-      if (res.ok) {
-        res
-          .json()
-          .then((res) => {
-            localStorage.setItem("token", res.data.api_token);
-            window.location.href = "/product_page";
-         
-          })
-          .catch((err) => {
-            // console.log(err);
-            let msg = "Something went wrong";
-            setErrorMessage(msg);
-          });
-      } else
-        res
-          .json()
-          .then((res) => {
-            errorMessage = res?.error?.message;
-            setErrorMessage(errorMessage);
-          })
-          .catch((err) => {
-            // console.log(err);
-            let msg = "Something went wrong";
-            setErrorMessage(msg);
-          });
-    })
-    .catch((err) => {
-      let msg = "Something went wrong";
-      setErrorMessage(msg);
-    });
-}
-
-function setErrorMessage(msg) {
-  errorContainer.innerText = `${msg}`;
-}
    
